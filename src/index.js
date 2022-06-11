86904c4241b9340a925ceeb9659d893c1ec126f3
Kelvin Etori <kelvinkamau1130@yahoo.com>
2022-06-11 14:56:16 -0700

    inital setup

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "/src/styles/main.css";
+
+const API_KEY = "a7384ac1b9096cc582d9e0c3d465f999";
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Weather App",
+      favicon: "./src/assets/cloud.png",
+      template: "./src/template.html",
+    }), // svg works too!
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+};
