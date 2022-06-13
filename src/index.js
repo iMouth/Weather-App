76dd9c88f41adbc45554f77a4d10211fb13b53db
Kelvin Etori <kelvinkamau1130@yahoo.com>
2022-06-12 18:03:53 -0700

    getting city info

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import "/src/styles/main.css";
 const API_KEY = "a7384ac1b9096cc582d9e0c3d465f999";
 const SEARCH_LIMIT = 10;
 
-function getSearch(city) {
-  const search = "http://api.openweathermap.org/geo/1.0/direct?q=" 
-  + city + "&limit=" + SEARCH_LIMIT + "&appid=" + API_KEY;
-  return search
+function getSearchLocations(city) {
+  return "http://api.openweathermap.org/geo/1.0/direct?q=" 
+        + city + "&limit=" + SEARCH_LIMIT + "&appid=" + API_KEY;
 }
 
 async function getLocations(city) {
-    let search = getSearch(city);
+    let search = getSearchLocations(city);
     try {
         let locationsCall = await fetch(search, {mode: 'cors'});
         let locationsJSON = await locationsCall.json();
@@ -20,7 +19,7 @@ async function getLocations(city) {
                 city: location.name,
                 state: location.state,
                 country: location.country,
-                logitude: location.lon,
+                longitude: location.lon,
                 latitude: location.lat
             };
             locations.push(info);
@@ -32,16 +31,44 @@ async function getLocations(city) {
     }
 }
 
+
 async function searchClick() {
-    let city = "tucson" // TODO: get search from DOM
+    let city = "seoul" // TODO: get search from DOM
     let locations = await getLocations(city);
     // TODO: Display Locations to user
+    let cityInfo = await cityClick(locations[0]);
+    console.log(cityInfo);
+}
 
-    cityClick(locations[0])
+function getSearchCity(city) {
+    return "https://api.openweathermap.org/data/2.5/weather?" + "lat=" + city.latitude 
+    + "&lon=" + city.longitude + "&units=imperial" + "&appid=" + API_KEY;
 }
 
 async function cityClick(city) {
-    console.log(city);
+    // TOOD: Get city from a global locations array
+    let search = getSearchCity(city);
+    try {
+        let cityCall = await fetch(search, {mode: 'cors'});
+        let cityJSON = await cityCall.json();
+        console.log(cityJSON);
+        let info = {
+            name: cityJSON.name,
+            temp: cityJSON.main.temp,
+            tempMax: cityJSON.main.temp_max,
+            tempMin: cityJSON.main.temp_min,
+            humidity: cityJSON.main.humidity,
+            tempFeel: cityJSON.main.feels_like,
+            windSpeed: cityJSON.wind.speed,
+            weather: cityJSON.weather[0].main,
+            weatherDesc: cityJSON.weather[0].description,
+            weatherIcon: cityJSON.weather[0].icon,
+        }
+        return info;
+    }
+    catch (err) {
+        console.log(err)
+    }
 }
 
 searchClick();
