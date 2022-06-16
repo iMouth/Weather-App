38811afb3d1f3d0e15dc3b60becdda0574606be2
Kelvin Etori <kelvinkamau1130@yahoo.com>
2022-06-16 02:52:45 -0700

    added dividers, polished some css, added pictures for weather

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(|avif|png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
         
       },
